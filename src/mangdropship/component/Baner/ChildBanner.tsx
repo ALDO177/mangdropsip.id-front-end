9f5be102425828743de0556a91e2c3262de3850e
ALDO177 <aldo.ratmawan9999@gmail.com>
2023-05-14 20:22:50 +0700

    Merubah file Component Baner/ChildBanner.tsx

--- src/mangdropship/component/Baner/ChildBanner.tsx
+++ src/mangdropship/component/Baner/ChildBanner.tsx
@@ -11,18 +11,28 @@ export default class ChildBanner extends React.Component{
 
     componentDidMount(): void {
         (async() =>{
-            new Splide('.splide', {
-                arrows: false,
-                pagination: false,
-                autoWidth: true,
-            });
+            const splides = document.querySelectorAll('.splides');
+            splides.forEach((element) => {
+                new Splide(element.className, {
+                    arrows: false,
+                    pagination: false,
+                    autoWidth: true,
+                });
+            })
         })();
     }
 
     render(): React.ReactNode {
         return (
             <>
-                <section className="splide" aria-label="Splide Basic HTML Example" style={{padding: '0px'}}>
+                <section className="splides" aria-label="Splide Basic HTML Example" style={{padding: '0px'}}>
+                    <div className="splide__track">
+                        <ul className="splide__list">
+                            <li className="splide__slide"><img src={banner} alt="Image 1"/></li>
+                        </ul>
+                    </div>
+                </section>
+                <section className="splides mt-2" aria-label="Splide Basic HTML Example" style={{padding: '0px'}}>
                     <div className="splide__track">
                         <ul className="splide__list">
                             <li className="splide__slide"><img src={banner} alt="Image 1"/></li>
